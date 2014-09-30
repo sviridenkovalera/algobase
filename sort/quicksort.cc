fe5034340ed6d6168ba5ae8c2df573dbe38d76a4
valera <cowr.valera@gmail.com>
2014-10-01 02:26:28 +0400

    add quicksort algorithm

---
+++ sort/quicksort.cc
@@ -0,0 +1,23 @@
+template <typename T>
+void quicksort(T *array, int left, int right)
+{
+	T middle = array[(left + right) >> 1], tmp;
+	int i = left, j = right;
+	
+	while (i <= j)
+	{
+		while (array[i] < middle) i++;
+		while (array[j] > middle) j--;
+
+		if (i <= j)
+		{
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			i++, j--;
+		}
+	}
+
+	if (left < j) quicksort(array, left, j);
+	if (i < right) quicksort(array, i, right);
+}
